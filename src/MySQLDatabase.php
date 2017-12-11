a4bb111599d5e657f0f8c60401e734139bbb50bd
Hoben <bargaoui.habib@gmail.com>
2017-12-11 12:46:03 +0100

    Add files via upload

---
+++ src/MySQLDatabase.php
@@ -0,0 +1,42 @@
+<?php
+namespace Hobben\Auth;
+
+class MySQLDatabase implements Database
+{
+    protected $data_server;
+    /**
+     * Get the value of data_server
+     */
+    public function getDataServer()
+    {
+        return $this->data_server;
+    }
+
+    /**
+     * Set the value of data_server
+     *
+     * @return  self
+     */
+    public function setDataServer($data_server)
+    {
+        $this->data_server = $data_server;
+
+        return $this;
+    }
+
+    /**
+     * connects to database
+     *
+     * @return  
+     */
+    public function connect()
+    {
+        echo ("hii");
+        echo ($this->getDataServer()->getHost());
+    }
+
+    public function __construct(){
+        $data_server = new \Hobben\Auth\DataServer(\HOST, \DATABASE_NAME, \USER_NAME, \USER_PASSWORD);
+        $this->data_server = $data_server;
+    }
+}
