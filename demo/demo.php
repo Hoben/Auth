ff9fc57619165db638c516d983a4cd91a1a40cf9
DESKTOP-HABIB\barga <bargaoui.habib@gmail.com>
2017-12-11 17:29:34 +0100

    in progress

--- demo/demo.php
+++ demo/demo.php
@@ -2,11 +2,13 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Hoben\Auth\MysqlDatabase;
+use Hoben\Auth\Session;
 
 define("HOST", "localhost");
-define("DATABASE_NAME", "opac_web_schema");
+define("DATABASE_NAME", "hoben-auth");
 define("USER_NAME", "root");
 define("USER_PASSWORD", "");
+define("USER_TABLE", "user-auth");
 
 $db = new MySQLDatabase();
 $db->connect();
